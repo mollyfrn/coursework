7143b6d7a417665530e5c5fc944a336a03f09ed7
Molly Jenkins <mollyfrn@live.unc.edu>
2017-02-27 18:06:09 -0500

    hw

---
+++ ENEC563/JenkinsA5.R
@@ -0,0 +1,8 @@
+#Molly Jenkins
+#Assignment 5
+#ENEC563 
+#02/27/2017
+
+#setwd(C:/git/coursework/ENEC563/)
+
+fisheggs = read.txt('https://sakai.unc.edu/access/content/group/7d7a0e1c-4adb-4ee2-ace8-490a89313a59/Data/fisheggs.txt', header = TRUE)
---
+++ ENEC563/Jenkins_A4.R
@@ -0,0 +1,8 @@
+#Molly Jenkins
+#Assignment 4
+#ENEC563 
+#02/27/2017
+
+#setwd(C:/git/coursework/ENEC563/)
+
+body = read.csv('mammalianbody.csv', header = TRUE)
