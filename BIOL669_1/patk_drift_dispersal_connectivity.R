d743130fd03f1e1b00bb43cab25741da66a5c6a9
Molly Jenkins <mollyfrn@live.unc.edu>
2017-03-14 17:37:32 -0400

    updates, downloaded OP version to compare

--- BIOL669_1/patk_drift_dispersal_connectivity.R
+++ BIOL669_1/patk_drift_dispersal_connectivity.R
@@ -1,96 +1,76 @@
-
-speciesnum = c(1:5)
-
-for(n in speciesnum){ #outside of workings bc both dealing with same n's
-
 ## specify parameters, initial conditions, and output matrix
 num.years <- 50 
 num.patch <- 10
-freq.mat.n <- matrix(nrow = num.years, ncol = num.patch) #each row is a year 
-#collapse into dataframe/single 50x10 matrix since redundant
+freq.1.mat <- matrix(nrow = num.years, ncol = num.patch)
+freq.2.mat <- matrix(nrow = num.years, ncol = num.patch)
+freq.3.mat <- matrix(nrow = num.years, ncol = num.patch)
+freq.4.mat <- matrix(nrow = num.years, ncol = num.patch)
+freq.5.mat <- matrix(nrow = num.years, ncol = num.patch)
 
 
-#collapse into single init val 
 J <- 100 # number of individuals PER PATCH
-init.n <- 0.2*J 
-
-
+init.1 <- 0.2*J 
+init.2 <- 0.2*J
+init.3 <- 0.2*J
+init.4 <- 0.2*J
+init.5 <- 0.2*J
 COM <- matrix(nrow=J, ncol=num.patch)
-COM[1:init.n,] <- 1; 
-COM[(init.n+1):(0.4*J),] <- 2; 
-COM[(0.4*J+1):(0.6*J),] <- 3; 
-COM[(0.6*J+1):(0.8*J),] <- 4; 
-COM[(0.8*J+1):J,] <- 5
+COM[1:init.1,] <- 1; COM[(init.1+1):(0.4*J),] <- 2; COM[(0.4*J+1):(0.6*J),] <- 3; COM[(0.6*J+1):(0.8*J),] <- 4; COM[(0.8*J+1):J,] <- 5
 
 year <- 2 
 
 m <- 0.2
-fit.ratio.avg <- c(1:(length =num.patch))
+fit.ratio.avg <- vector(length=num.patch)
 fit.ratio.avg[] <- 1
-freq.dep <- c(1:(length=num.patch))
+freq.dep <- vector(length=num.patch)
 freq.dep[] <- 0
 
-
-#collapse to be nested 
 ## record data (frequency of species X) for year 1
-freq.mat.n[1,] <- init.n/J 
-#deleted extraneous matrices bc all the same 1st column
-
-
-#make vectors of to and from outside of loop 
-frompatch.id = c(1:(length=num.patch)) # vector of possible emigration patches (each patch has a unique integer for its identity)
-topatch.id = c(1:(length=num.patch)) # vector of possible immigration patches (same IDs as above)
-
+freq.1.mat[1,] <- init.1/J 
+freq.2.mat[1,] <- init.2/J 
+freq.3.mat[1,] <- init.3/J 
+freq.4.mat[1,] <- init.4/J 
+freq.5.mat[1,] <- init.5/J 
 
 ## run simulation
 for (i in 1:(J*num.patch*(num.years-1))) {
-    ## define connectivity for dispersal events 
-    patch.prob <- (sample(frompatch.id,1,replace=TRUE)/sample(topatch.id,1,replace=TRUE)) 
-  # ratio of em-/immigration patch IDs = 
-  # probability of successful dispersal bewtween the two randomly selected patches above 
-  # (frompatch.id and topatch.id)
   
-    
+  ## define connectivity for dispersal events 
+  frompatch.id = c(1,2,3,4,5,6,7,8,9,10) # vector of possible emigration patches (each patch has a unique integer for its identity)
+  topatch.id = c(1,2,3,4,5,6,7,8,9,10) # vector of possible immigration patches (same IDs as above)
+  patch.prob <- (sample(frompatch.id,1,replace=TRUE)/sample(topatch.id,1,replace=TRUE)) # ratio of em-/immigration patch IDs = 
+  # probability of successful dispersal bewtween the two randomly selected patches above (frompatch.id and topatch.id)
+  
   ## choose a patch where a death even will occur
   patch <- sample(1:num.patch,1)
   
-  
-  ## calculate Pr.n if dispersal occurs
-    if (runif(1) < (m*patch.prob)) {  
-    # multiplying disperal rate (m) by patch.prob gives final probability of successful dispersal
-   
-      Pr.n <- sum(COM==n)/(J*num.patch)
-      output = data.frame(Pr.n, final_Pr)
+  ## calculate Pr.X if dispersal occurs
+  if (runif(1) < (m*patch.prob)) {  # multiplying disperal rate (m) by patch.prob gives final probability of successful dispersal
+    Pr.1 <- sum(COM==1)/(J*num.patch)
+    Pr.2 <- sum(COM==2)/(J*num.patch)
+    Pr.3 <- sum(COM==3)/(J*num.patch)
+    Pr.4 <- sum(COM==4)/(J*num.patch)
+    Pr.5 <- sum(COM==5)/(J*num.patch)
+  } else { 
     
-    } else { 
-    
-      ## calculate Pr.n if local reproduction (not dispersal)
-      Pr.n <- sum(COM[,patch]==n)/J; 
-    }
-    #need to label and configure output storage
-    #can create single df of probabilities as output from loop since running thru them anyway
-
+    ## calculate Pr.X if local reproduction (not dispersal)
+    freq.1 <- sum(COM[,patch]==1)/J; freq.2 <- sum(COM[,patch]==2)/J; freq.3 <- sum(COM[,patch]==3)/J; freq.4 <- sum(COM[,patch]==4)/J; freq.5 <- sum(COM[,patch]==5)/J
+    Pr.1 <- freq.1; Pr.2 <- freq.2; Pr.3 <- freq.3; Pr.4 <- freq.4; Pr.5 <- freq.5
+  }
   
-  
-  #could do another forloop for n 1:5 and Pr.n of 1:5 since aligned
-  COM[ceiling(J*runif(1)),patch] <- sample(n, 1, prob=Pr.n[n]) 
-  
-
+  COM[ceiling(J*runif(1)),patch] <- sample(c(1,2,3,4,5), 1, prob=c(Pr.1,Pr.2,Pr.3,Pr.4,Pr.5)) 
   
   ## record data  
   if (i %% (J*num.patch) == 0) {
-    freq.mat[year,] <- colSums(COM==n)/J
+    freq.1.mat[year,] <- colSums(COM==1)/J
+    freq.2.mat[year,] <- colSums(COM==2)/J
+    freq.3.mat[year,] <- colSums(COM==3)/J
+    freq.4.mat[year,] <- colSums(COM==4)/J
+    freq.5.mat[year,] <- colSums(COM==5)/J
     year <- year + 1 
-    n <- n
   }
 } 
 
-}
-
-
-
-
-#come back to this later
 ## graph the results
 par(mfrow=c(2,3))
 plot(1:num.years, freq.1.mat[,1], type="l", xlab="Time", 
