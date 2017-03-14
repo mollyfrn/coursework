9bdd386199a32ffcad611585402af2e45f9a381d
Molly Jenkins <mollyfrn@live.unc.edu>
2017-03-14 18:26:51 -0400

    updates, downloaded OP version to compare

--- BIOL669_1/mfj_drift_dispersal_connectivity.R
+++ BIOL669_1/mfj_drift_dispersal_connectivity.R
@@ -25,11 +25,6 @@ COM[(0.8*J+1):J,] <- 5
 year <- 2 
 
 m <- 0.2
-fit.ratio.avg <- c(1:(length =num.patch))
-fit.ratio.avg[] <- 1
-freq.dep <- c(1:(length=num.patch))
-freq.dep[] <- 0
-
 
 #collapse to be nested 
 ## record data (frequency of species X) for year 1
@@ -37,22 +32,18 @@ freq.mat.n[1,] <- init.n/J
 #deleted extraneous matrices bc all the same 1st column
 
 
-#make vectors of to and from outside of loop 
-frompatch.id = c(1:(length=num.patch)) # vector of possible emigration patches (each patch has a unique integer for its identity)
-topatch.id = c(1:(length=num.patch)) # vector of possible immigration patches (same IDs as above)
+
+#I think this patch.prob needs to be max 1 otherwise there is a chance that m*patch.prob is over 1?
 
 
 ## run simulation
 for (i in 1:(J*num.patch*(num.years-1))) {
-    ## define connectivity for dispersal events 
-    patch.prob <- (sample(frompatch.id,1,replace=TRUE)/sample(topatch.id,1,replace=TRUE)) 
-  # ratio of em-/immigration patch IDs = 
-  # probability of successful dispersal bewtween the two randomly selected patches above 
-  # (frompatch.id and topatch.id)
-  
-    
-  ## choose a patch where a death even will occur
-  patch <- sample(1:num.patch,1)
+    ## choose a patch where a death even will occur
+    #make vectors of to and from outside of loop 
+    from.patches.vector=c(1:num.patch)
+    patch <- sample(1:num.patch,1) #patch where death occurs
+    from.patch.sample<-sample( from.patches, 1, replace=TRUE)
+    patch.prob<-min(patch, from.patch.sample)/max(patch, from.patch.sample)
   
   
   ## calculate Pr.n if dispersal occurs
@@ -87,14 +78,11 @@ for (i in 1:(J*num.patch*(num.years-1))) {
 
 }
 
-
-
-
 #come back to this later
 ## graph the results
 par(mfrow=c(2,3))
-plot(1:num.years, freq.1.mat[,1], type="l", xlab="Time", 
-     ylab="Frequency of species 1", ylim=c(0,1))
+plot(1:num.years, freq.mat.n[,1], type="l", xlab="Time", 
+     ylab=paste("Frequency of species", n, sep = ""), ylim=c(0,1))
 for (i in 2:(num.patch)) {
   lines(1:num.years,freq.1.mat[,2], type="l", col="red", ylim=c(0,1))
   lines(1:num.years,freq.1.mat[,3], type="l", col="blue", ylim=c(0,1))
@@ -105,56 +93,4 @@ for (i in 2:(num.patch)) {
   lines(1:num.years,freq.1.mat[,8], type="l", col="gray", ylim=c(0,1))
   lines(1:num.years,freq.1.mat[,9], type="l", col="brown", ylim=c(0,1))
   lines(1:num.years,freq.1.mat[,10], type="l", lty=2, ylim=c(0,1))
-}
-plot(1:num.years, freq.2.mat[,1], type="l", xlab="Time", 
-     ylab="Frequency of species 2", ylim=c(0,1))
-for (i in 2:(num.patch)) {
-  lines(1:num.years,freq.2.mat[,2], type="l", col="red", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,3], type="l", col="blue", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,4], type="l", col="green", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,5], type="l", col="yellow", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,6], type="l", col="purple", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,7], type="l", col="orange", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,8], type="l", col="gray", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,9], type="l", col="brown", ylim=c(0,1))
-  lines(1:num.years,freq.2.mat[,10], type="l", lty=2, ylim=c(0,1))
-}
-plot(1:num.years, freq.3.mat[,1], type="l", xlab="Time", 
-     ylab="Frequency of species 3", ylim=c(0,1))
-for (i in 2:(num.patch)) {
-  lines(1:num.years,freq.3.mat[,2], type="l", col="red", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,3], type="l", col="blue", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,4], type="l", col="green", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,5], type="l", col="yellow", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,6], type="l", col="purple", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,7], type="l", col="orange", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,8], type="l", col="gray", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,9], type="l", col="brown", ylim=c(0,1))
-  lines(1:num.years,freq.3.mat[,10], type="l", lty=2, ylim=c(0,1))
-}
-plot(1:num.years, freq.4.mat[,1], type="l", xlab="Time", 
-     ylab="Frequency of species 4", ylim=c(0,1))
-for (i in 2:(num.patch)) {
-  lines(1:num.years,freq.4.mat[,2], type="l", col="red", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,3], type="l", col="blue", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,4], type="l", col="green", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,5], type="l", col="yellow", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,6], type="l", col="purple", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,7], type="l", col="orange", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,8], type="l", col="gray", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,9], type="l", col="brown", ylim=c(0,1))
-  lines(1:num.years,freq.4.mat[,10], type="l", lty=2, ylim=c(0,1))
-}
-plot(1:num.years, freq.5.mat[,1], type="l", xlab="Time", 
-     ylab="Frequency of species 5", ylim=c(0,1))
-for (i in 2:(num.patch)) {
-  lines(1:num.years,freq.5.mat[,2], type="l", col="red", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,3], type="l", col="blue", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,4], type="l", col="green", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,5], type="l", col="yellow", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,6], type="l", col="purple", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,7], type="l", col="orange", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,8], type="l", col="gray", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,9], type="l", col="brown", ylim=c(0,1))
-  lines(1:num.years,freq.5.mat[,10], type="l", lty=2, ylim=c(0,1))
 }
