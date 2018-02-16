95c9619d9a4e0203e690be05c84f8778f4ed3773
Molly <mollyfrn@live.unc.edu>
2018-02-16 12:52:53 -0500

    creating script outline to make map for Margaret

---
+++ Starlings_forMargaret.R
@@ -0,0 +1,23 @@
+### Author: Molly Jenkins 
+### Date: 02/16/2018 
+### This script takes the entire North American Breeding Bird Survey and subsets it to just Starling presence/absence over time. 
+### This script also plots Starling data over time and space for the duration of the entire BBS. 
+
+#setwd("C:/git/core-transient") 
+library(tidyverse) 
+library(ecodataretriever) 
+
+####Download BBS data at single route resolution, subset to just starlings (EUST; STUVUL)#### 
+
+
+
+
+####Plot North American continent with locations with Starling presence in at least 1 year####
+
+
+
+
+####Extra: Over span of years included, color code routes by frequency of Starlings -> Green = every year, yellow = infrequently####
+
+
+#Send to Margaret along with BBS data citation and R citation! 
