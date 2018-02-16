a6cba10a1dc5d3f29875bba2b99ca8fcb53b74c6
Molly <mollyfrn@live.unc.edu>
2018-02-16 12:57:19 -0500

    added some pseudo-code

--- Starlings_forMargaret.R
+++ Starlings_forMargaret.R
@@ -6,15 +6,19 @@
 #setwd("C:/git/core-transient") 
 library(tidyverse) 
 library(ecodataretriever) 
+BBS = "bioark.ad.unc.edu/Hurlbertlab/Jenkins/BBS/"
 
 ####Download BBS data at single route resolution, subset to just starlings (EUST; STUVUL)#### 
 
+bbs = read.csv("bbs.csv", header = TRUE)
 
+stars = bbs %>% 
+  filter(AOU == "EUST" | AOU == "STUVUL")
 
 
 ####Plot North American continent with locations with Starling presence in at least 1 year####
-
-
+NorthAm = 
+routes = read.csv("bbs_routes.csv", header = TRUE)
 
 
 ####Extra: Over span of years included, color code routes by frequency of Starlings -> Green = every year, yellow = infrequently####
